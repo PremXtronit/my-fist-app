e730b35cef5392f66652bfd6fc0d89d538a1dc72
prem8 <prem85chandra@gmail.com>
2020-05-24 00:23:54 +0530

    Change titles and styling on homepage

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  enterTitle = 'this will bind to title variable in html page';
+}
